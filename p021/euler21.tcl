a3ca8b890851865b33ec51eed5ff68fcd6d5797e
Thomas Wright <vorduga@gmail.com>
2013-03-13 17:19:30 -0400

    Problem 21 in Tcl

---
+++ p021/euler21.tcl
@@ -0,0 +1,42 @@
+# Project Euler Problem 21
+# 
+# Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+# which divide evenly into n).
+#
+# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and
+# each of a and b are called amicable numbers.
+# 
+# For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44,
+# 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4,
+# 71 and 142; so d(284) = 220.
+# 
+# Evaluate the sum of all the amicable numbers under 10000.
+
+proc sum_divisors {n} {
+    set sum 0
+    for {set i 1} {$i < $n} {incr i} {
+        if {($n % $i) == 0} {
+            incr sum $i
+        }
+    }
+    return $sum
+}
+
+proc is_amicable {n} {
+    set sd [sum_divisors $n]
+    set sd2 [sum_divisors $sd]
+    return [expr ($n == $sd2) && ($n != $sd)]
+}
+
+proc sum_amicables {n} {
+    set sum 0
+    for {set i $n} {$i > 0} {incr i -1} {
+        if {[is_amicable $i]} {
+            incr sum $i
+        }
+    }
+    return $sum
+}
+
+puts [sum_amicables 10000]
+
