a7ce2bb1a6597402febf76b8b462d15fd60d05d5
Tom Wright <tom_wright@fastmail.fm>
2014-12-22 13:15:52 -0500

    Problem 52 in Haskell

---
+++ p052/e52.hs
@@ -0,0 +1,25 @@
+-- #52
+--
+-- It can be seen that the number, 125874, and its double, 251748, contain
+-- exactly the same digits, but in a different order.
+--
+-- Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+-- contain the same digits.
+
+import Data.List
+
+digits' 0 = []
+digits' n = last : digits' (div n 10)
+    where last = n `mod` 10
+
+digits n = reverse $ digits' n
+
+sameDigits n m = (sort (digits n)) == (sort (digits m))
+
+multIsPermutation n m = sameDigits n (n * m)
+
+sixMatch n = all (multIsPermutation n) [1..6]
+
+e52 = find sixMatch [1..]
+
+main = putStrLn $ show $ e52
